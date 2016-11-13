d478854ae4fd02b1c005cc182f4741195efd291e
Masayuki MIZUNO <mizuno@sf.ecei.tohoku.ac.jp>
2016-11-13 15:46:56 +0900

    Add tail duplication in assoc.ml

--- assoc.ml
+++ assoc.ml
@@ -2,17 +2,14 @@
 
 open KNormal
 
-let rec f = function (* ネストしたletの簡約 (caml2html: assoc_f) *)
-  | IfEq(x, y, e1, e2) -> IfEq(x, y, f e1, f e2)
-  | IfLE(x, y, e1, e2) -> IfLE(x, y, f e1, f e2)
-  | Let(xt, e1, e2) -> (* letの場合 (caml2html: assoc_let) *)
-      let rec insert = function
-        | Let(yt, e3, e4) -> Let(yt, e3, insert e4)
-        | LetRec(fundefs, e) -> LetRec(fundefs, insert e)
-        | LetTuple(yts, z, e) -> LetTuple(yts, z, insert e)
-        | e -> Let(xt, e, f e2) in
-      insert (f e1)
-  | LetRec({ name = xt; args = yts; body = e1 }, e2) ->
-      LetRec({ name = xt; args = yts; body = f e1 }, f e2)
-  | LetTuple(xts, y, e) -> LetTuple(xts, y, f e)
-  | e -> e
+let rec g e k = (* ネストしたletの簡約 (caml2html: assoc_g) *)
+  match e with
+  | IfEq (x, y, e1, e2) -> IfEq (x, y, g e1 k, g e2 k)
+  | IfLE (x, y, e1, e2) -> IfLE (x, y, g e1 k, g e2 k)
+  | Let (xt, e1, e2) -> g e1 (fun e1' -> Let (xt, e1', g e2 k))
+  | LetRec ({ body = e1 } as fundef, e2) ->
+      LetRec ({ fundef with body = g e1 (fun e1' -> e1') }, g e2 k)
+  | LetTuple (xts, y, e) -> LetTuple (xts, y, g e k)
+  | e -> k e
+
+let f e = g e (fun e' -> e')
