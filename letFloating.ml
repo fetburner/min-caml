1c56e47266d63c337229c10c3145d5a8528f5342
Masayuki MIZUNO <mizuno@sf.ecei.tohoku.ac.jp>
2016-11-16 02:55:42 +0900

    Refactor let-floating

--- letFloating.ml
+++ letFloating.ml
@@ -1,44 +1,45 @@
 open KNormal
 
-let ( ||| ) o f =
-  match o with
-  | Some _ -> o
-  | None -> f ()
-
-(* collect (innermost) floatable binding *)
-let rec collect bv ctx = function
+(* collect floatable bindings *)
+let rec collect bv e k =
+  match e with
   | IfEq (x, y, e1, e2) ->
-      collect bv (fun e1' -> ctx (IfEq (x, y, e1', e2))) e1 ||| (fun () ->
-        collect bv (fun e2' -> ctx (IfEq (x, y, e1, e2'))) e2)
+      collect bv e1 (fun bv e1' ->
+        collect bv e2 (fun bv e2' ->
+          k bv (IfEq (x, y, e1', e2'))))
   | IfLE (x, y, e1, e2) ->
-      collect bv (fun e1' -> ctx (IfLE (x, y, e1', e2))) e1 ||| (fun () ->
-        collect bv (fun e2' -> ctx (IfLE (x, y, e1, e2'))) e2)
+      collect bv e1 (fun bv e1' ->
+        collect bv e2 (fun bv e2' ->
+          k bv (IfLE (x, y, e1', e2'))))
   | Let ((x, t), e1, e2) ->
-      collect bv (fun e1' -> ctx (Let ((x, t), e1', e2))) e1 ||| (fun () ->
-        if S.subset (fv e1) bv && not (effect e1) then Some (x, t, e1, ctx e2)
-        else collect bv (fun e2' -> ctx (Let ((x, t), e1, e2'))) e2)
+      collect bv e1 (fun bv e1' ->
+        if S.subset (fv e1') bv && not (effect e1') then
+          (Format.eprintf "floating %s@." x;
+           Let ((x, t), e1', collect (S.add x bv) e2 k))
+        else
+          collect bv e2 (fun bv e2' ->
+            k bv (Let ((x, t), e1', e2'))))
   | LetRec ({ body = e1 } as fundef, e2) ->
-      collect bv (fun e1' -> ctx (LetRec ({ fundef with body = e1' }, e2))) e1 ||| (fun () ->
-        collect bv (fun e2' -> ctx (LetRec (fundef, e2'))) e2)
+      collect bv e1 (fun bv e1' ->
+        collect bv e2 (fun bv e2' ->
+          k bv (LetRec ({ fundef with body = e1' }, e2'))))
   | LetTuple (xts, y, e) ->
-      collect bv (fun e' -> ctx (LetTuple (xts, y, e'))) e
-  | _ -> None
+      collect bv e (fun bv e' ->
+        k bv (LetTuple (xts, y, e')))
+  | e -> k bv e
 
 (* let-floating *)
 (* variable names must be unique *)
-let rec f bv = function
-  | IfEq (x, y, e1, e2) -> IfEq (x, y, f bv e1, f bv e2)
-  | IfLE(x, y, e1, e2) -> IfLE(x, y, f bv e1, f bv e2)
-  | Let ((x, t), e1, e2) -> Let ((x, t), f bv e1, f (S.add x bv) e2)
+let rec g bv e = collect bv e (fun bv -> function
+  | IfEq (x, y, e1, e2) -> IfEq (x, y, g bv e1, g bv e2)
+  | IfLE(x, y, e1, e2) -> IfLE (x, y, g bv e1, g bv e2)
+  | Let ((x, t), e1, e2) -> Let ((x, t), g bv e1, g (S.add x bv) e2)
   | LetRec ({ name = (x, _); args = yts; body = e1 } as fundef, e2) ->
-      (match collect bv (fun e1' -> LetRec ({ fundef with body = e1' }, e2)) e1 with
-       | None ->
-           LetRec ({ fundef with body =
-                       f (S.union bv (S.of_list (x :: List.map fst yts))) e1 },
-                   f (S.add x bv) e2)
-       | Some (x, t, e1, e2) -> Let ((x, t), e1, f (S.add x bv) e2))
+      LetRec ({ fundef with body =
+                  g (S.union bv (S.of_list (x :: List.map fst yts))) e1 },
+              g (S.add x bv) e2)
   | LetTuple (xts, y, e) ->
-      LetTuple (xts, y, f (S.union bv (S.of_list (List.map fst xts))) e)
-  | e -> e
+      LetTuple (xts, y, g (S.union bv (S.of_list (List.map fst xts))) e)
+  | e -> e)
 
-let f = f S.empty
+let f = g S.empty
